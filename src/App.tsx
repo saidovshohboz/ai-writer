5a1ea9d96cb357a7ccb990929b0b16cfb46a2508
saidovshohboz <shohbozsaidovv@gmail.com>
2024-08-13 16:02:37 +0500

    init: Project initiated and configured

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './components/ui/select';
+
+function App() {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+}
+
+export default App;
